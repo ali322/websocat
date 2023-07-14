c212b282430e8fe85402879403392b109f3c5792
chenli13 <chenli13@asiainfo.com>
2023-07-14 21:40:35 +0800

    fix pass

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { join }  from 'path'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { join } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/websocat/",
   resolve: {
     alias: {
-      '@': join(__dirname, 'src')
-    }
+      "@": join(__dirname, "src"),
+    },
   },
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [AntDesignVueResolver()]
+      resolvers: [AntDesignVueResolver()],
     }),
     Components({
-      resolvers: [AntDesignVueResolver()]
-    })
-  ]
-})
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
+});
